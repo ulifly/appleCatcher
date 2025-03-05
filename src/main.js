f4ba4366b6b807291ae0786e63b43bffb7cfe628
Ulifly <ulifly@hotmail.com>
2025-03-04 21:03:46 -0600

    13 38

--- src/main.js
+++ src/main.js
@@ -15,22 +15,44 @@ class GameScene extends Phaser.Scene {
     this.player
     this.cursors
     this.playerSpeed = speedDown + 50
+    this.target
+    this.points = 0
   }
 
   preload() {
     this.load.image("bg", 'assets/bg.png')
     this.load.image("basket", 'assets/basket.png')
+    this.load.image("apple", 'assets/apple.png')  
   }
   create(){
     this.add.image(0, 0, "bg").setOrigin(0, 0)
-    this.player = this.physics.add.image(0, sizes.height -100, "basket").setOrigin(0, 0)
+
+    this.player = this.physics.add
+    .image(0, sizes.height -100, "basket")
+    .setOrigin(0, 0)
+
     this.player.setCollideWorldBounds(true)
     this.player.setImmovable()
     this.player.body.allowGravity = false
+    this.player.body.setSize(100, 15)
+
+    this.target = this.physics.add
+    .image(this.getRandomX(), 0, "apple")
+    .setOrigin(0, 0)
+    this.target.setMaxVelocity(0, speedDown)
+
+    this.physics.add.collider(this.player, this.target, this.targetHit, null, this)
 
     this.cursors = this.input.keyboard.createCursorKeys()
   }
   update(){
+
+    if(this.target.y > sizes.height){
+      this.target.y = 0
+      this.target.x = this.getRandomX()
+    }
+
+
     const { left, right } = this.cursors;
     if(left.isDown){
       this.player.setVelocityX(-this.playerSpeed)
@@ -41,7 +63,17 @@ class GameScene extends Phaser.Scene {
       this.player.setVelocityX(0)
   }
 
+  getRandomX(){
+    return Math.floor(Math.random() * sizes.width)
+  }
+
+  targetHit(){
+    this.points++
+    this.target.setY(0)
+    this.target.setX(this.getRandomX())
+  }
 }
+
 
 const config = {
   type: Phaser.WEBGL,
